e04b0121442daf56efe4fd32fec104511225582b
FrolovBI <bi50_d.s.frolov@mpt.ru>
2020-03-06 13:22:22 +0300

    My cool File manager v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(23, 119);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(754, 308);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 44);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 40);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Up";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(442, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 90);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ReadOnly = true;
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(754, 23);
+            this.metroTextBox1.TabIndex = 2;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(149, 44);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(86, 40);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Копировать";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(241, 44);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(87, 40);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "Вставить";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(334, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileManager
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        string path_to_copy;
+        int papki;
+        string now_path;
+        bool now_select_disk;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void go(string where)
+        {
+            treeView1.Nodes.Clear();
+            now_path = where;
+            metroTextBox1.Text = now_path;
+            string[] folders = Directory.GetDirectories(@where);
+            string[] files = Directory.GetFiles(@where);
+            papki = 0;
+            foreach(string foldername in folders)
+            {
+                treeView1.Nodes.Add(foldername);
+                papki++;
+            }
+            foreach (string filename in files)
+                treeView1.Nodes.Add(filename);
+        }
+
+        private void disks()
+        {
+            now_select_disk = true;
+            papki = 0;
+            now_path = "";
+            metroTextBox1.Text = now_path;
+            treeView1.Nodes.Clear();
+            DriveInfo[] names = DriveInfo.GetDrives();
+            foreach (DriveInfo name in names)
+            {
+                treeView1.Nodes.Add(name.Name);
+                papki++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            now_select_disk = true;
+            disks();
+
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (treeView1.SelectedNode.Index < papki && !now_select_disk)
+            {
+                go(treeView1.SelectedNode.Text.ToString());
+            } else if (now_select_disk)
+            {
+                disks();
+                now_select_disk = false;
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (now_path == @"C:\" || now_path=="") disks();
+            else if (now_path.LastIndexOf('\\')==2) go(now_path.Substring(0,3));
+            else go(now_path.Substring(0, now_path.LastIndexOf('\\')));
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            treeView1.Select();
+            if(treeView1.SelectedNode.Index>=papki)
+            path_to_copy = treeView1.SelectedNode.Text.ToString();
+            metroLabel1.Text = "Копируется файл " + path_to_copy.Substring(path_to_copy.LastIndexOf('\\') + 1);
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(now_path +'\\'+ path_to_copy.Substring(path_to_copy.LastIndexOf('\\') + 1)))
+            {
+                File.Copy(path_to_copy, now_path+'\\'+path_to_copy.Substring(path_to_copy.LastIndexOf('\\') + 1), true);
+                metroLabel1.Text = "Файл успешно скопирован!";
+                go(now_path);
+            }
+            else metroLabel1.Text = "Произошла ошибка при копировании файла!";
+            
+        }
+    }
+}
